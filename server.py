d4b1e554796288b12bb7a0643a43d9fc12ef4ada
Tella <tellarinipietro@gmail.com>
2023-02-03 09:13:25 +0100

    First tipe of implementation

---
+++ server.py
@@ -0,0 +1,30 @@
+import socket
+import _thread as thread
+
+
+def main():
+
+    def run():
+        print("Starting")
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        host = '127.0.0.1'
+        port = 8000
+
+        s.bind((host, port))
+        s.listen()
+
+        while True:
+            client, addr = s.accept()
+
+            print("* new connection from %s" % str(addr))
+
+            msg = 'Good connection'+'\r\n'
+            client.send(msg.encode('ascii'))
+            client.close
+    run()
+
+
+if __name__ == '__main__':
+    print("execute")
+    main()
